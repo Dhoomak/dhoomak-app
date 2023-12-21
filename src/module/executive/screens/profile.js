ba73359988b1a207f4d9c8adaebadc84d84e4fde
Dhoomak <dhoomak@Dhoomaks-MacBook-Air.local>
2023-12-21 18:57:44 +0530

    landscape disabled, onboarding details fixed, user data added in profile need to show the data

--- src/module/executive/screens/profile.js
+++ src/module/executive/screens/profile.js
@@ -1,33 +1,69 @@
-import React from 'react';
-import { View, Text, Image, StyleSheet, TextInput } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text, Image, StyleSheet, TextInput} from 'react-native';
 import IMAGES from '../../../assets/images';
 import COLORS from '../../../utils/color';
 import Icon from 'react-native-vector-icons/Ionicons';
+import {getAsyncStorageObjectItem} from '../../../utils/async-storage';
+import {ASYNC_STORAGE_KEY} from '../../../data/constant';
 
 const Profile = () => {
+  const [data, setData] = useState([]);
   const userData = {
     name: 'John Doe',
     email: 'john.doe@example.com',
     address: '123 Main St, Cityville',
     dob: '01/01/1990',
-    id:12333,
+    id: 12333,
   };
+
+  const getUserData = async () => {
+    try {
+      const userData = await getAsyncStorageObjectItem(
+        ASYNC_STORAGE_KEY.USER_DATA,
+      );
+      setData(userData);
+      console.log(userData, 'agent data');
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    }
+  };
+  useEffect(() => {
+    const fetchData = async () => {
+      await getUserData();
+    };
+    fetchData();
+    return () => {};
+  }, []);
 
   return (
     <View style={styles.container}>
       <View style={styles.profileContainer}>
-        <View style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
-            <Image style={styles.avatar} source={IMAGES.comingSoon} />
-            <Text style={styles.name}>{userData.name}</Text>
-            <Text style={styles.id} className="text-red">Employee Id:{userData.id}</Text>
+        <View
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Image style={styles.avatar} source={IMAGES.comingSoon} />
+          <Text style={styles.name}>{userData.name}</Text>
+          <Text style={styles.id} className="text-red">
+            Employee Id:{userData.id}
+          </Text>
         </View>
         <View className="flex flex-col gap-1 mt-5">
-            <Text style={styles.input} className="py-2 px-3">Email: {userData.email}</Text>
-            <Text style={styles.input} className="py-2 px-3">Address: {userData.address}</Text>
-            <Text style={styles.input} className="py-2 px-3">Date of Birth: {userData.dob}</Text>
-            <Text style={styles.input} className="py-2 px-3">Number: {userData.dob}</Text>
+          <Text style={styles.input} className="py-2 px-3">
+            Email: {userData.email}
+          </Text>
+          <Text style={styles.input} className="py-2 px-3">
+            Address: {userData.address}
+          </Text>
+          <Text style={styles.input} className="py-2 px-3">
+            Date of Birth: {userData.dob}
+          </Text>
+          <Text style={styles.input} className="py-2 px-3">
+            Number: {userData.dob}
+          </Text>
         </View>
-
       </View>
     </View>
   );
@@ -42,7 +78,7 @@ const styles = StyleSheet.create({
   avatar: {
     width: 100,
     height: 100,
-    marginVertical:20,
+    marginVertical: 20,
     backgroundColor: 'red',
   },
   name: {
@@ -50,19 +86,16 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 5,
   },
-  input:{
+  input: {
     fontSize: 16,
     // fontWeight: 'bold',
-    color:"#403B3B",
+    color: '#403B3B',
     marginBottom: 5,
-    borderBottomWidth:0.5,
-  }
+    borderBottomWidth: 0.5,
+  },
 });
 
 export default Profile;
-
-
-
 
 // ionicons/MaterialCommunityIcons
 // email-outline,call,location-pin
