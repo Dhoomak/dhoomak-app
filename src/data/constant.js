ebf992f7d3aab6d1bbd42b25f91083cec3a40281
jivesh <jsoperatorz@gmail.com>
2023-12-29 22:15:07 +0530

    Changed url of api

--- src/data/constant.js
+++ src/data/constant.js
@@ -1,6 +1,7 @@
 export const APP_NAME = 'Dhoomak';
 export const OTP_LENGTH = 4;
-export const BASE_API_URL = 'http://ec2-13-232-182-101.ap-south-1.compute.amazonaws.com/';
+export const BASE_API_URL = 'https://apis.prod.dhoomak.com';
+// export const BASE_API_URL = 'http://ec2-13-232-182-101.ap-south-1.compute.amazonaws.com/';
 
 export const ROLE = {
   RESTAURANT: 'RESTAURANT',
